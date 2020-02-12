6d435577dcca214a12e2350a5f793f84c5a5353c
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-12 15:20:54 +0100

    added new endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 // src/index.ts
 import "reflect-metadata";
 import { createKoaServer } from "routing-controllers";
-import Controller from "./controller";
+import PageController from "./pages/controller";
 const port = process.env.PORT || 4000;
 const app = createKoaServer({
-  controllers: [Controller]
+  controllers: [PageController]
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
